22830ba41c2dff9c912b8a6e6529512619ab35ce
Weina Ma <wma@ca.teraproc.com>
2016-08-26 22:35:07 +0000

    add warning state to alert

--- package/scripts/alerts/alert_mapred_long_job.py
+++ package/scripts/alerts/alert_mapred_long_job.py
@@ -2,7 +2,16 @@ import requests
 import json
 
 RESULT_STATE_OK = 'OK'
+RESULT_STATE_WARNING = 'WARNING'
 RESULT_STATE_CRITICAL = 'CRITICAL'
+
+# threshold
+THRESHOLD_CRITICAL = 100
+THRESHOLD_WARNING = 50
+
+# the interval to check the metric, default is 1 minute
+INTERVAL_PARAM_KEY = 'interval'
+INTERVAL_PARAM_DEFAULT = 1
 
 def execute(configurations={}, parameters={}, host_name=None):
     """
@@ -15,30 +24,65 @@ def execute(configurations={}, parameters={}, host_name=None):
     """
 
     url = 'http://localhost:9200/logstash-mapred/_search?'
-    payload = json.loads('{"fields" : ["appId"], "query" : {"range" : {"executionTime":{"gte": "100"}, "@timestamp": {"gt" : "now-1m"}}}}')
+    interval = INTERVAL_PARAM_DEFAULT
+    if INTERVAL_PARAM_KEY in parameters:
+      interval = _coerce_to_integer(parameters[INTERVAL_PARAM_KEY])
+      
+    # critical applications
+    payload = json.loads('{"fields" : ["appId"], "query" : {"bool":  {"must": [ {"and": [{"range" : {"executionTime":{"gte": %d}}}, {"range": {"@timestamp": {"gt" : "now-%dm"}}}]}]}}}'%(THRESHOLD_CRITICAL,interval))
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     r = requests.post(url, data=json.dumps(payload), headers=headers)
     output = r.json()
     
-    errors = []
-    apps = []
+    msgs = []
+    err_apps = []
     if "hits" in output and "hits" in output["hits"] and len(output["hits"]["hits"]) > 0:
         for item in output["hits"]["hits"]:
             appId = item["fields"]["appId"][0]
             if appId is not None:
-                apps.append(appId)
+                err_apps.append(appId)
                 
-    if len(apps) > 0:
-        errors.append("The executionTime of applications '{0}' is greater than 100 seconds.".format(", ".join(apps)))
+    if len(err_apps) > 0:
+        msgs.append("The executionTime of applications '{0}' is greater than {1} seconds.".format(", ".join(err_apps), THRESHOLD_CRITICAL))
+        
+    # warning applications
+    payload = json.loads('{"fields" : ["appId"], "query" : {"bool":  {"must": [ {"and": [{"range" : {"executionTime":{"gte": %d, "lte": %d}}}, {"range": {"@timestamp": {"gt" : "now-%dm"}}}]}]}}}'%(THRESHOLD_WARNING, THRESHOLD_CRITICAL,interval))
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    output = r.json() 
+
+    warn_apps = []
+    if "hits" in output and "hits" in output["hits"] and len(output["hits"]["hits"]) > 0:
+        for item in output["hits"]["hits"]:
+            appId = item["fields"]["appId"][0]
+            if appId is not None:
+                warn_apps.append(appId)
+
+    if len(warn_apps) > 0:
+        msgs.append("; The executionTime of applications '{0}' is greater than {1} and less than {2}seconds.".format(", ".join(warn_apps), THRESHOLD_WARNING, THRESHOLD_CRITICAL))
     
     # Determine the status based on errors.    
-    if len(errors) == 0:
-        status = RESULT_STATE_OK
-        messages = []
-        return (status, ["\n".join(messages)])
-    else:
+    if len(err_apps) == 0 and len(warn_apps) == 0:
+        return (RESULT_STATE_OK, ["\n".join([])])
+    elif len(err_apps) > 0:
         # Report errors
-        return (RESULT_STATE_CRITICAL, ["\n".join(errors)])
+        return (RESULT_STATE_CRITICAL, ["\n".join(msgs)])
+    else:
+        # Report warnings
+        return (RESULT_STATE_WARNING, ["\n".join(msgs)])
+        
+def _coerce_to_integer(value):
+  """
+  Attempts to correctly coerce a value to an integer. For the case of an integer or a float,
+  this will essentially either NOOP or return a truncated value. If the parameter is a string,
+  then it will first attempt to be coerced from a integer, and failing that, a float.
+  :param value: the value to coerce
+  :return: the coerced value as an integer
+  """
+  try:
+    return int(value)
+  except ValueError:
+    return int(float(value))
         
 if __name__ == "__main__":
     result = execute()
