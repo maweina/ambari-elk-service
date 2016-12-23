91a84485d469d25575e50ee8690110779db7ceb4
zhaoyim <yyyiming@163.com>
2016-12-23 11:37:20 +0800

    Fix some issues 1. Add import kibana index script 2. Remove package condition check in ambari meta xml 3. Change alter json OCCI version to 2.5 4. Change all ELK to OCCI 5. Change logstash 0+ to ALL in ambari meta xml 6. Remove the graphs jsons, because use graphs json j2

--- package/scripts/kibana.py
+++ package/scripts/kibana.py
@@ -95,3 +95,9 @@ def kibana(role=None):
           group=params.kibana_user_group,
           mode=0755
     )
+    File(format("{kibana_home}/kibana-create-index-patterns.sh"),
+         content=Template(format("kibana-create-index-patterns.sh.j2")),
+         owner=params.kibana_user,
+         group=params.kibana_user_group,
+         mode=0755
+         )
