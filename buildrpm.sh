a2118844e23e3350c1ec8e0b7ce04da7432a22cb
Qian Guo <guoqian3@asiainfo.com>
2017-01-04 15:01:23 +0800

    fix bug

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 OCCI_PATH=`dirname $0`
 cd $OCCI_PATH
+mkdir -pv dist/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS}
 mkdir dist/SOURCES/OCCI
 cp -rf src/* dist/SOURCES/OCCI
 tar -zcvf dist/SOURCES/occi-1.0.0.0.tar.gz -C dist/SOURCES OCCI
