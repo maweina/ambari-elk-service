c900b4fb26cd3822589d946a436934414b57a33a
郭茜 <guoqian3@asiainfo.com>
2017-02-15 13:43:14 +0800

    fix bug of hardcoded es http port

--- src/package/scripts/alerts/alert_mapred_long_job.py
+++ src/package/scripts/alerts/alert_mapred_long_job.py
@@ -4,7 +4,8 @@ import json
 RESULT_STATE_OK = 'OK'
 RESULT_STATE_WARNING = 'WARNING'
 RESULT_STATE_CRITICAL = 'CRITICAL'
-
+RESULT_CODE_UNKNOWN = 'UNKNOWN'
+RESULT_STATE_UNKNOWN = 'UNKNOWN'
 # threshold
 MR_EXECTIME_WARNING_THRESHOLD_DEFAULT = 7200
 MR_EXECTIME_CRITICAL_THRESHOLD_DEFAULT = 3600
@@ -14,7 +15,13 @@ MR_EXECTIME_CRITICAL_THRESHOLD_KEY = "mr.exectime.critical.threshold"
 # the interval to check the metric, default is 1 minute
 INTERVAL_PARAM_KEY = 'interval'
 INTERVAL_PARAM_DEFAULT = 1
-
+ES_PORT = '{{elasticsearch-site/http.port}}'
+def get_tokens():
+    """
+    Returns a tuple of tokens in the format {{site/property}} that will be used
+    to build the dictionary passed into execute
+    """
+    return (ES_PORT,)
 def execute(configurations={}, parameters={}, host_name=None):
     """
     Returns a tuple containing the result code and a pre-formatted result label
@@ -24,8 +31,12 @@ def execute(configurations={}, parameters={}, host_name=None):
     parameters (dictionary): a mapping of script parameter key to value
     host_name (string): the name of this host where the alert is running
     """
-
-    url = 'http://localhost:9200/mylogstash-yarn-apps/_search?'
+    if configurations is None:
+        return (RESULT_CODE_UNKNOWN, ['There were no configurations supplied to the script.'])
+    if set([ES_PORT]).issubset(configurations):
+        url = 'http://localhost:' + configurations[ES_PORT] + '/mylogstash-yarn-apps/_search?'
+    else:
+        return (RESULT_CODE_UNKNOWN, ['The es_port is a required parameter.'])
     interval = INTERVAL_PARAM_DEFAULT
     if INTERVAL_PARAM_KEY in parameters:
       interval = _coerce_to_integer(parameters[INTERVAL_PARAM_KEY])
@@ -35,12 +46,15 @@ def execute(configurations={}, parameters={}, host_name=None):
     mr_exectime_threshold_critical = MR_EXECTIME_CRITICAL_THRESHOLD_DEFAULT
     if MR_EXECTIME_CRITICAL_THRESHOLD_KEY in parameters:
         mr_exectime_threshold_critical =_coerce_to_integer(parameters[MR_EXECTIME_CRITICAL_THRESHOLD_KEY])
+
     # critical applications
     payload = json.loads('{"fields" : ["appId"], "query" : {"bool":  {"must": [ {"and": [{"range" : {"elapsedTime":{"gte": %d}}}, {"range": {"finishedTime": {"gt" : "now-%dm"}}}]}]}}}'%(mr_exectime_threshold_critical,interval))
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    try:
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+    except:
+        return (RESULT_STATE_UNKNOWN, ['Connection to elasticsearch failed.'])
     output = r.json()
-    
     msgs = []
     err_apps = []
     if "hits" in output and "hits" in output["hits"] and len(output["hits"]["hits"]) > 0:
@@ -48,23 +62,23 @@ def execute(configurations={}, parameters={}, host_name=None):
             appId = item["fields"]["appId"][0]
             if appId is not None:
                 err_apps.append(appId)
-                
     if len(err_apps) > 0:
         msgs.append("The execution time of MapReduce Job '{0}' is greater than {1} seconds.".format(", ".join(err_apps), mr_exectime_threshold_critical))
         
     # warning applications
     payload = json.loads('{"fields" : ["appId"], "query" : {"bool":  {"must": [ {"and": [{"range" : {"elapsedTime":{"gte": %d, "lte": %d}}}, {"range": {"finishedTime": {"gt" : "now-%dm"}}}]}]}}}'%(mr_exectime_threshold_warning, mr_exectime_threshold_critical,interval))
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
-    output = r.json() 
-
+    try:
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+    except:
+        return (RESULT_STATE_UNKNOWN, ['Connection to elasticsearch failed.'])
+    output = r.json()
     warn_apps = []
     if "hits" in output and "hits" in output["hits"] and len(output["hits"]["hits"]) > 0:
         for item in output["hits"]["hits"]:
             appId = item["fields"]["appId"][0]
             if appId is not None:
                 warn_apps.append(appId)
-
     if len(warn_apps) > 0:
         msgs.append("The execution time of MapReduce Job '{0}' is greater than {1} seconds and less than {2} seconds.".format(", ".join(warn_apps), mr_exectime_threshold_warning, mr_exectime_threshold_critical))
     
